45ce3fdb5e6bd7217be3dde2b402e6b6da4410c3
baxtergu <baxtergu@gmail.com>
2017-03-08 15:40:37 +0800

    添加message service测试代码

--- src/app/services/messages.service.spec.ts
+++ src/app/services/messages.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { MessagesService } from './messages.service';
+import { User, Thread, Message } from '../models/models';
 
 describe('MessagesService', () => {
   beforeEach(() => {
@@ -9,7 +10,33 @@ describe('MessagesService', () => {
     });
   });
 
-  it('should ...', inject([MessagesService], (service: MessagesService) => {
+  it('should test', inject([MessagesService], (service: MessagesService) => {
+    let user: User = new User('Nate', '');
+    let thread: Thread = new Thread('t1', 'Nate', '');
+
+    let m1: Message = new Message({
+      author: user,
+      text: 'Hi!',
+      thread: thread
+    });
+
+    let m2: Message = new Message({
+      author: user,
+      text: 'Bye!',
+      thread: thread
+    });
+
+    service.newMessages.subscribe((message: Message) => {
+      console.log('=> messages： ' + message.text);
+    });
+
+    service.messages.subscribe((messages: Message[]) => {
+      console.log('=> messages： ' + messages.length);
+    });
+
+    service.addMessage(m1);
+    service.addMessage(m2);
+
     expect(service).toBeTruthy();
   }));
 });
